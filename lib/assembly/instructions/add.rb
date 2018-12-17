2e72dcb9dbb2edf819c32f02a2e5a56b99274df5
Luca Nioi <lnioi@thoughtworks.com>
2018-12-17 17:31:03 +0100

    Implement add instruction

---
+++ lib/assembly/instructions/add.rb
@@ -0,0 +1,20 @@
+module Assembly
+  module Instructions
+    class Add
+      def initialize(target_register, source_register)
+        @target_register = target_register
+        @source_register = source_register
+      end
+
+      def execute(program)
+        value = program.get_register(source_register) + program.get_register(target_register)
+        program.set_register(target_register, value)
+        program.proceed
+      end
+
+      private
+
+      attr_reader :target_register, :source_register
+    end
+  end
+end
--- spec/assembly/instructions/mov_spec.rb
+++ spec/assembly/instructions/mov_spec.rb
@@ -2,19 +2,30 @@ describe Assembly::Instructions::Mov do
   subject(:mov_instruction) { described_class.new(target_register, source_register) }
   let(:target_register) { :a }
   let(:source_register) { :b }
+
   let(:program) { Assembly::Program.new(instruction_set, registry) }
   let(:registry) { Assembly::Registry.new }
   let(:instruction_set) { double(:instruction_set) }
 
   before do
-    program.set_register(source_register, 90)
+    program.set_register(source_register, 88) if source_register.is_a? Symbol
   end
 
   describe '#execute' do
     it 'copies the value in to the register' do
       subject.execute(program)
 
-      expect(program.get_register(target_register)).to eq 90
+      expect(program.get_register(target_register)).to eq 88
+    end
+
+    context 'when source register is just a raw-value integer' do
+      let(:source_register) { 100 }
+
+      it 'copies the integer into the register' do
+        subject.execute(program)
+
+        expect(program.get_register(target_register)).to eq 100
+      end
     end
 
     it 'increments the instruction pointer' do
@@ -27,3 +38,5 @@ describe Assembly::Instructions::Mov do
     end
   end
 end
+
+
